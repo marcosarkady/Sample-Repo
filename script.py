03109aa295afec33e97fa5e3fb232438ee0c303f
Marco Sarkady <m.sarkady@outlook.de>
2021-08-05 10:49:40 +0200

    changed code

--- script.py
+++ script.py
@@ -1,27 +1,7 @@
 import sys
-import pyperclip
-
-# git
-# source d:/DesktopD/myscript/start/venv/Scripts/activate
-# powershell
-# d:/DesktopD/myscript/start/venv/Scripts/activate.ps1
-
-# strg+shift+P
-
-# python -m venv ProjektName
-# d:/DesktopD/myscript/start/venv/Scripts/Activate.ps1
-# deactivate
-# rmdir ProjektName /s (for subtrees)
-
-# python -m venv projektname --system-site-packages
-# pip list --local
-# pip freeze
-# pip install -r requirements.txt
-
 
 print(sys.version)
 print(sys.executable)
-pyperclip.copy("Hey")
 
 
 def greet(who_to_greet):
